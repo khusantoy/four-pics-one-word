2527f93d11b67bd07d01e84ee176a18df2e3404f
Khusanboy Tursunov <tursunovkhusanboy@gmail.com>
2024-07-30 16:33:19 +0500

    fixed bugs

--- lib/controllers/word_controller.dart
+++ lib/controllers/word_controller.dart
@@ -3,99 +3,29 @@ import 'dart:math';
 import 'package:get/get.dart';
 
 class WordController extends GetxController {
-  RxInt inCorrectAnswers = 0.obs;
-  RxInt correctAnswers = 0.obs;
-
   RxInt currentQuestion = 0.obs;
 
-  List<Map<String, dynamic>> questions = [
-    {
-      'question': "Bozordan bir oldim. Uyga ming keldim.",
-      'answer': ['a', 'n', 'o', 'r']
-    },
-    {
-      'question': "Yerdagi to'q sariq tirnoq.",
-      'answer': ['s', 'a', 'b', 'z', 'i']
-    },
-    {
-      'question': "Ortib, achchiq qizil meva, Oq dengizda suzar kema.",
-      'answer': ['d', 'a', 'z', 'm', 'o', 'l']
-    },
-    {
-      'question': "Tinmas bitta, Tinglar mingta.",
-      'answer': ['r', 'a', 'd', 'i', 'o']
-    },
-    {
-      'question': "Sen ichida tursang tikka, Olib chiqar yuksaklikka.",
-      'answer': ['l', 'i', 'f', 't']
-    },
-    {
-      'question': "Olmasang yarim nafas, Yo'ldan o'tgani qo'ymas.",
-      'answer': ['v', 'e', 'r', 'g', 'u', 'l']
-    },
-    {
-      'question': "Odamzodda nima ko'p?",
-      'answer': ['u', 'm', 'i', 'd']
-    },
-    {
-      'question': "Suv qayerda ustundek turadi?",
-      'answer': ['q', 'u', 'd', 'u', 'q']
-    },
-    {
-      'question':
-          "Besh harfdan iborat bo'lgan sichqon ushlaydigan qopqonni topa olasizmi?",
-      'answer': ['m', 'u', 's', 'h', 'u', 'k']
-    },
-    {
-      'question':
-          "Kunlardan bir kun chol o'g'liga o'ttiz tiyin berib shunday debdi: — O'g'lim, shu pulga narsa olib kelginki, biz ham, tovug'imiz ham, qo'zimiz ham to'yadigan bo'lsin. Bola nima olib kelishi kerak?",
-      'answer': ['q', 'o', 'v', 'u', 'n']
-    }
-  ];
-
-  Map<String, dynamic> get question {
-    return questions[currentQuestion.value];
-  }
-
-  List<String> get answers {
+  List<String> getAnswer(String word) {
     const String alphabet = 'abcdefghijklmnopqrstuvwxyz';
     Random random = Random();
     List<String> randomLetters = [];
 
-    for (int i = 0;
-        i < 10 - questions[currentQuestion.value]['answer'].length;
-        i++) {
+    for (int i = 0; i < 12 - word.length; i++) {
       int randomIndex = random.nextInt(alphabet.length);
       randomLetters.add(alphabet[randomIndex]);
     }
-    List<String> combinedList = [
-      ...randomLetters,
-      ...questions[currentQuestion.value]['answer']
-    ];
+    List<String> combinedList = [...randomLetters, ...word.split('')];
 
     combinedList.shuffle();
 
     return combinedList;
   }
 
-  void incrementInCorrect() {
-    inCorrectAnswers++;
-  }
-
-  void incrementCorrect() {
-    correctAnswers++;
-  }
-
   void nextQuestion() {
-    if (currentQuestion.value != questions.length - 1) {
+    if (currentQuestion.value != 4 - 1) {
       currentQuestion++;
     } else {
       currentQuestion.value = 0;
     }
-  }
-
-  void reset() {
-    correctAnswers = 0.obs;
-    inCorrectAnswers = 0.obs;
   }
 }
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:four_pics_one_word/blocs/puzzle/puzzle_bloc.dart';
 import 'package:four_pics_one_word/controllers/word_controller.dart';
@@ -14,6 +13,77 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final wordController = Get.put(WordController());
+  int currentUserAnswer = 0;
+  List<String> userAnswer = [];
+  List<String> answers = [];
+
+  void _handleAnswerTap(int index, String word) {
+    setState(() {
+      userAnswer[currentUserAnswer] = answers[index];
+      answers[index] = '';
+
+      if (userAnswer.join('') == word && !userAnswer.contains('')) {
+        Future.delayed(const Duration(milliseconds: 500), () {
+          showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (context) {
+              return const AlertDialog(
+                title: Text("Correct 👍"),
+              );
+            },
+          );
+        });
+
+        Future.delayed(const Duration(seconds: 2), () {
+          Navigator.of(context).pop();
+        });
+
+        Future.delayed(const Duration(seconds: 1), () {
+          context.read<PuzzleBloc>().add(NextPuzzleEvent());
+          _resetPuzzle(word);
+        });
+      } else if (!userAnswer.contains('') && userAnswer.join('') != word) {
+        Future.delayed(const Duration(milliseconds: 500), () {
+          showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (context) {
+              return const AlertDialog(
+                title: Text("Incorrect 👎"),
+              );
+            },
+          );
+        });
+
+        Future.delayed(const Duration(seconds: 2), () {
+          Navigator.of(context).pop();
+        });
+
+        Future.delayed(const Duration(seconds: 1), () {
+          context.read<PuzzleBloc>().add(NextPuzzleEvent());
+          _resetPuzzle(word);
+        });
+      }
+
+      if (currentUserAnswer < userAnswer.length - 1) {
+        currentUserAnswer++;
+      } else {
+        currentUserAnswer = 0;
+      }
+    });
+  }
+
+  void _resetPuzzle(String word) {
+    answers.clear();
+    userAnswer.clear();
+    currentUserAnswer = 0;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,78 +93,68 @@ class _HomeScreenState extends State<HomeScreen> {
         child: BlocBuilder<PuzzleBloc, PuzzleState>(
           bloc: context.read<PuzzleBloc>()..add(GetPuzzleEvent()),
           builder: (context, state) {
-            print(state);
             if (state is LoadedPuzzleState) {
               final puzzle = state.puzzle;
               final images = puzzle['images'];
               final word = puzzle['word'];
 
-              int currentUserAnswer = 0;
+              if (userAnswer.isEmpty && answers.isEmpty) {
+                userAnswer = List.generate(word.length, (index) => '');
+                answers = wordController.getAnswer(word);
+              }
 
-              List<String> userAnswer = [];
-              List<String> answers = [];
-
-              userAnswer = List.generate(
-                word.length,
-                (index) {
-                  return '';
-                },
-              );
               return Column(
                 children: [
                   SizedBox(
                     height: 450,
-                    child: Expanded(
-                      child: GridView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 10,
-                          mainAxisSpacing: 10,
-                        ),
-                        itemCount: 4,
-                        itemBuilder: (context, index) {
-                          final image = images[index];
-                          return Container(
-                            width: 200,
-                            height: 200,
-                            decoration: BoxDecoration(
-                              border: Border.all(width: 4, color: Colors.amber),
-                            ),
-                            child: Image.asset(
-                              image,
-                              fit: BoxFit.cover,
-                            ),
-                          );
-                        },
+                    child: GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
                       ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: List.generate(
-                      word.length,
-                      (index) {
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        final image = images[index];
                         return Container(
-                          width: 60,
-                          height: 60,
+                          width: 200,
+                          height: 200,
                           decoration: BoxDecoration(
-                            border: Border.all(width: 4),
+                            border: Border.all(width: 4, color: Colors.amber),
                           ),
-                          child: Center(
-                            child: Text(userAnswer[index].toUpperCase()),
+                          child: Image.asset(
+                            image,
+                            fit: BoxFit.cover,
                           ),
                         );
                       },
                     ),
                   ),
-                  const SizedBox(
-                    height: 40,
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: List.generate(word.length, (index) {
+                      return Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 4, color: Colors.blue),
+                        ),
+                        child: Center(
+                          child: Text(
+                            userAnswer[index].toUpperCase(),
+                            style: const TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
                   ),
+                  const SizedBox(height: 40),
                   Expanded(
                     child: GridView.builder(
                       gridDelegate:
@@ -106,141 +166,19 @@ class _HomeScreenState extends State<HomeScreen> {
                       itemCount: answers.length,
                       itemBuilder: (context, index) {
                         return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              userAnswer[currentUserAnswer] = answers[index];
-                              answers[index] = '';
-
-                              if (userAnswer.join('') == word &&
-                                  !userAnswer.contains('')) {
-                                wordController.incrementCorrect();
-                                Get.defaultDialog(
-                                  barrierDismissible: false,
-                                  title: "Correct 👍",
-                                );
-                                wordController.nextQuestion();
-                                if (wordController.currentQuestion.value == 0) {
-                                  Future.delayed(const Duration(seconds: 4),
-                                      () {
-                                    Get.defaultDialog(
-                                      barrierDismissible: false,
-                                      title: "Your statistics",
-                                      content: Column(
-                                        children: [
-                                          Text(
-                                              "Correct answer: ${wordController.correctAnswers}"),
-                                          Text(
-                                              "Incorrect answer: ${wordController.inCorrectAnswers}")
-                                        ],
-                                      ),
-                                      cancel: TextButton(
-                                          onPressed: () {
-                                            SystemNavigator.pop();
-                                          },
-                                          child: const Text("Exit")),
-                                      confirm: FilledButton(
-                                        onPressed: () {
-                                          setState(() {
-                                            wordController.reset();
-                                            Get.back();
-                                          });
-                                        },
-                                        child: const Text("Again"),
-                                      ),
-                                    );
-                                  });
-                                }
-                                answers = wordController.answers;
-                                userAnswer = List.generate(
-                                  wordController.question['answer'].length,
-                                  (index) {
-                                    return '';
-                                  },
-                                );
-                                Future.delayed(const Duration(seconds: 3), () {
-                                  Get.back();
-                                });
-                                currentUserAnswer = -1;
-                              } else if (!userAnswer.contains('') &&
-                                  userAnswer.join('') !=
-                                      wordController.question['answer']
-                                          .join('')) {
-                                Get.defaultDialog(
-                                  barrierDismissible: false,
-                                  title: "Incorrect 👎",
-                                  content: Image.asset("assets/gifs/no.gif"),
-                                );
-                                wordController.nextQuestion();
-                                if (wordController.currentQuestion.value == 0) {
-                                  Future.delayed(const Duration(seconds: 4),
-                                      () {
-                                    Get.defaultDialog(
-                                      barrierDismissible: false,
-                                      title: "Your statistics",
-                                      content: Column(
-                                        children: [
-                                          Text(
-                                              "Correct answer: ${wordController.correctAnswers}"),
-                                          Text(
-                                              "Incorrect answer: ${wordController.inCorrectAnswers}")
-                                        ],
-                                      ),
-                                      cancel: TextButton(
-                                          onPressed: () {
-                                            SystemNavigator.pop();
-                                          },
-                                          child: const Text("Exit")),
-                                      confirm: FilledButton(
-                                        onPressed: () {
-                                          setState(() {
-                                            wordController.reset();
-                                            Get.back();
-                                          });
-                                        },
-                                        child: const Text("Again"),
-                                      ),
-                                    );
-                                  });
-                                }
-                                wordController.incrementInCorrect();
-                                answers = wordController.answers;
-                                userAnswer = List.generate(
-                                  wordController.question['answer'].length,
-                                  (index) {
-                                    return '';
-                                  },
-                                );
-                                Future.delayed(const Duration(seconds: 3), () {
-                                  Get.back();
-                                });
-                                currentUserAnswer = -1;
-                              }
-
-                              if (currentUserAnswer < userAnswer.length - 1) {
-                                currentUserAnswer++;
-                              } else {
-                                currentUserAnswer = 0;
-                              }
-                            });
-                          },
+                          onTap: () => _handleAnswerTap(index, word),
                           child: answers[index] == ''
                               ? const SizedBox()
-                              : SizedBox(
-                                  width: 55,
-                                  height: 55,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.lightGreen,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: Center(
-                                      child: Text(
-                                        answers[index].toUpperCase(),
-                                        style: const TextStyle(
-                                          fontSize: 28,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                        ),
+                              : Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(width: 4),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      answers[index].toUpperCase(),
+                                      style: const TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.bold,
                                       ),
                                     ),
                                   ),
@@ -248,8 +186,17 @@ class _HomeScreenState extends State<HomeScreen> {
                         );
                       },
                     ),
-                  )
+                  ),
                 ],
+              );
+            }
+
+            if (state is CompletedPuzzleState) {
+              return const Center(
+                child: Text(
+                  "Game Over",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
               );
             }
 
